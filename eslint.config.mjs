48360ea0bea7030619e62fd0731c7f41481bcb04
Md. Alamin A <md.alamin.ali.096@gmail.com>
2024-08-17 22:05:53 +0600

    bug fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,6 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-
   {
     languageOptions: {
       globals: {
